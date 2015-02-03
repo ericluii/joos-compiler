e914b144a7f9e8cfa098bc89fa1ea98c62908256
Eric Lui <ealui@uwaterloo.ca>
2015-02-03 14:12:44 -0500

    Created a clean parse tree print with indentation

--- src/parseTree.cpp
+++ src/parseTree.cpp
@@ -9,6 +9,22 @@ void ParseTree::addChild(ParseTree* child) {
     children.push_back(child);
 }
 
+void ParseTree::print(int depth) {
+    for (int i = 0; i < depth; i++) {
+        std::cout << ' ';
+    }
+
+    std::cout << treeLexeme << ":" << rule;
+    if (token != NULL) {
+        std::cout << ":" << token->getString();
+    }
+    std::cout << std::endl;
+
+    for (unsigned int i = 0; i < children.size(); i++) {
+        children[i]->print(depth + 1);
+    }
+}
+
 std::ostream& operator<<(std::ostream& os, const ParseTree& parseTree) {
     os << parseTree.treeLexeme;
     if(parseTree.token == NULL) {
