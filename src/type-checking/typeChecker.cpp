f72cadc2162c02ffc8acb57c76188e1e6f3886d5
Trevor Kitt <trevor_kitt@hotmail.com>
2015-03-16 17:49:22 -0400

    fix boolean casting check

--- src/type-checking/typeChecker.cpp
+++ src/type-checking/typeChecker.cpp
@@ -569,7 +569,7 @@ bool TypeChecking::check(CastExpression* castExpression){
             //std::cout << "Error on casting " << paramaterExpression->getExpressionTypeString() << " to Primitive type" << std::endl;
             Error(E_DEFAULT, NULL, "[DEV NOTE - REPLACE] cannot cast reference type to primitive type");
         }
-        else if(paramaterExpression->isExprTypeBoolean())
+        else if(paramaterExpression->isExprTypeBoolean() && (castExpression->getExprType() != ET_BOOLEAN))
         {
             Error(E_DEFAULT, NULL, "[DEV NOTE - REPLACE] cannot cast a boolean");
         }
