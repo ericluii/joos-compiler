e472190ed3e4f107437e3996ff85a4efc30a4ecb
Trevor Kitt <trevor_kitt@hotmail.com>
2015-03-31 19:25:16 -0400

    expand sequences

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -41,6 +41,8 @@ int Scanner::Scan(std::ifstream& file, std::vector<Token*> *tokens)
     int errorCount = 0;
     std::vector<int> errorFlags(numDfas, 0);
     std::pair<DFA_STATES, TOKEN_TYPE> result;
+    bool escapeSequence = false;
+    bool doneSequence = false;
 
     //Values for the token currently being scanned
     TOKEN_TYPE type = TT_INVALID;
@@ -81,6 +83,15 @@ int Scanner::Scan(std::ifstream& file, std::vector<Token*> *tokens)
             if (errorFlags[i] == 0) {
                 result = dfas[i]->transition((char)c);
                 switch (result.first) {
+                    case DS_ESCAPESINGLE:
+                    case DS_ESCAPEDOUBLE:
+                        escapeSequence = true;
+                        break;
+                    case DS_ENCLOSESINGLE:
+                    case DS_DOUBLEQUOTE:
+                        doneSequence = true;
+                        lexime += CharStringLiteralDfa::replacement;
+                        break;
                     case DS_ACCEPT:
                         type = result.second;
                         break;
@@ -139,7 +150,13 @@ int Scanner::Scan(std::ifstream& file, std::vector<Token*> *tokens)
                 errorFlags[i] = 0;
             }
         } else {
-            lexime += (char)c;
+            if(!escapeSequence){
+                lexime += (char)c;
+            }
+            if(doneSequence == true){
+                escapeSequence = false;
+                doneSequence = false;
+            }
             c = -2;
         }
     }
